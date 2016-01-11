d60471821bc679708aa9f8ece28939bc1c04713a
Andrzej Raczkowski <araczkowski@gmail.com>
2016-01-11 11:39:46 +0100

    introjs

--- Gruntfile.js
+++ Gruntfile.js
@@ -173,7 +173,7 @@ module.exports = function (grunt) {
                 files: {
                     src: [
                         '<%= config.dist %>/scripts/{,*/}*.js',
-                        '<%= config.dist %>/styles/{,*/}main.css',
+                        '<%= config.dist %>/styles/{,*/}*.css',
                         '<%= config.dist %>/images/{,*/}*.*',
                         '<%= config.dist %>/styles/fonts/{,*/}*.*',
                         '<%= config.dist %>/*.{ico,png}'
